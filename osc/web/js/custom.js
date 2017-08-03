266610049677147d8422d6a5d4d4faabd9821047
B-Matt <matej.arlovic@gmail.com>
2017-08-03 15:23:59 +0200

    Updated navigation & CSS fixes

--- osc/web/js/custom.js
+++ osc/web/js/custom.js
@@ -5,10 +5,24 @@ define([
         "use strict";
 
         $('.langs-wrapper').hover(function() {
-            $('.langs-others-container').toggle();
+            $('.langs-others-container').toggleClass('langs-others-container-inline');
         });
 
         $('.header > li a').last().click(function() {
             $('.header-mini').slideToggle("slow");
         });
+
+        $('.header > li a').mouseenter(function() {
+            $(this).prepend('<small class="effect-arrow">></small>').append('<span class="effect">_</span>');
+            setInterval(function() { dashBlink() }, 1000);
+            function dashBlink() {
+                $(".effect").fadeTo(700, 0.1).fadeTo(1000, 1.0);
+            }
+        });
+
+        $('.header > li a').mouseleave(function() {
+            $(this).find('.effect').remove();
+            $(this).find('.effect-arrow').remove();
+        });
+
     });
